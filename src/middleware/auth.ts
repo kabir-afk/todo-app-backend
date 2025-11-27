adb2c17f95145f06f13784294f4d04d24599a63a
Kabir Singh <kabirsingh12b@gmail.com>
2025-11-28 03:10:14 +0530

    chore : added auth and error middlewares

---
+++ src/middleware/auth.ts
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
+import { type Response, type Request, type NextFunction } from "express";
+import { env } from "../../config/env.js";
+
+interface JwtPayload {
+  _id: string;
+}
+
+async function isAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const token = req.cookies.token;
+    if (!token) res.json({ success: false, Message: "You are not logged in" });
+
+    const decoded = jwt.verify(token, env.JWT_SECRET_KEY) as JwtPayload;
+
+    req.user = await User.findById(decoded._id);
+
+    next();
+  } catch (error) {
+    console.error("Missing token \n", error);
+  }
+}
+
+export { isAuthenticated };
