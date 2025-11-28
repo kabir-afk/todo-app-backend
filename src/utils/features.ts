7a472227cc2ff2ed1b171badf662368dfd3caead
Kabir Singh <kabirsingh12b@gmail.com>
2025-11-28 20:21:04 +0530

    removed httpOnly cookie attribute

--- src/utils/features.ts
+++ src/utils/features.ts
@@ -12,7 +12,6 @@ function sendCookie(res: Response, user: IUser, message: string) {
   res
     .status(201)
     .cookie("token", token, {
-      httpOnly: true,
       maxAge: 15 * 60 * 1000,
       sameSite: env.NODE_ENV === "DEVELOPMENT" ? "lax" : "none",
       secure: env.NODE_ENV === "DEVELOPMENT" ? false : true,
