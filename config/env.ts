b21000a01cdf782ea9a831a0e96bd76bf26f2393
Kabir Singh <kabirsingh12b@gmail.com>
2025-11-29 16:26:41 +0530

    added gmail_user and gmail_api_password keys

--- config/env.ts
+++ config/env.ts
@@ -8,6 +8,8 @@ interface ENV {
   PORT: string;
   NODE_ENV: string;
   FRONTEND_URI: string;
+  GMAIL_USER: string;
+  GMAIL_API_PASSWORD: string;
 }
 
 export const env: ENV = {
@@ -16,4 +18,6 @@ export const env: ENV = {
   PORT: process.env.PORT!,
   NODE_ENV: process.env.NODE_ENV!,
   FRONTEND_URI: process.env.FRONTEND_URI!,
+  GMAIL_USER: process.env.GMAIL_API_USER!,
+  GMAIL_API_PASSWORD: process.env.GMAIL_API_PASS!,
 };
